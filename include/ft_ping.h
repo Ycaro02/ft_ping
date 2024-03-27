a68238daa0c91107ba165e46d922fbee4c2b6ef3
Ycaro02 <ycaro02@hotmailfr>
2024-03-27 16:56:12 +0100

    header comment

--- include/ft_ping.h
+++ include/ft_ping.h
@@ -9,6 +9,8 @@
 #include <errno.h>              /* Error number */
 #include <signal.h>             /* Signal handling */
 #include <fcntl.h>              /* File control */
+#include <time.h>				/* Time */
+
 #include "../include/basic_define.h" /* Basic define, include <sys/type*/
 #include "../libft/libft.h"     /* Libft library,*/
 
@@ -51,25 +53,23 @@ typedef struct iphdr t_iphdr;
 /* End of padding */
 #define DATA_END_PAD DATA_MBIDX + DATA_PAD_ZERO
 
-void gener_random_data(uint8_t *buff, int size);
 
-/* First used brut data can be remove */
-#define BRUT_DATA \
-{ 109, 243, 2, 102, 0, 0, 0, 0, 54, 209, 12, 0, 0, 0, 0, 0, \
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, \
-16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, \
-32, 33, 34, 35, 36, 37, 38, 39\
-}
-
+/**
+ * Context structure for ping
+*/
 typedef struct s_context
 {
-    t_sockaddr_in   dst_sockaddr;  /* Destination socket address */
-    in_addr_t       src_addr;    /* Source address */
+    t_sockaddr_in   dst_sockaddr;	/* Destination socket address */
+    in_addr_t       src_addr;		/* Source address */
     int             send_sock;      /* socket for sending */
     int             rcv_sock;       /* socket for receiving */
     uint16_t        flag;           /* ping flag */
-
 } t_context;
+
+// typedef struct s_ping_state
+// {
+
+// }	t_ping_state;
 
 /**
  * Packet structure for ping
@@ -82,13 +82,14 @@ typedef struct s_ping_packet
 } t_ping_packet;
 
 
-/* Signal handling variable */
+/* Signal handling global variable */
 extern int  g_signal_received;
 
 /* socket handle */
 int     close_socket(int sock);
 int     open_rcv_socket(void);
 int     open_send_socket(void);
+
 /* listen icmp reply */
 int8_t listen_icmp_reply(int sock);
 
@@ -98,3 +99,6 @@ t_ping_packet build_ping_packet(in_addr_t addr_from, in_addr_t addr_dest);
 /* checksum */
 uint16_t    compute_checksum(uint16_t *data, size_t size);
 uint8_t     verify_checksum(void *buffer, uint16_t ip_checksum, uint16_t icmp_checksum);
+
+/* random data */
+void gener_random_data(uint8_t *buff, int size);
