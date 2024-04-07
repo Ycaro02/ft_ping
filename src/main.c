dc574a93576538219054b8f17c27be51ece6e9ca
Ycaro02 <Ycaro02@hotmail.fr>
2024-04-07 19:54:49 +0200

    Use dprintf instead else if statement for display time

--- src/main.c
+++ src/main.c
@@ -104,13 +104,15 @@ void display_ms_time(char *color, suseconds_t time, uint8_t last)
 {
 	uint32_t mod = time % 1000;
 
-	if (mod < 100) {
-		ft_printf_fd(1, "%s%i.0%i", color, (time / 1000), mod);
-	} else if (mod < 10) {
-		ft_printf_fd(1, "%s%i.00%i", color, (time / 1000), mod);
-	} else {
-		ft_printf_fd(1, "%s%i.%i", color, (time / 1000), mod);
-	}
+	// if (mod < 100) {
+	// 	ft_printf_fd(1, "%s%i.0%i", color, (time / 1000), mod);
+	// } else if (mod < 10) {
+	// 	ft_printf_fd(1, "%s%i.00%i", color, (time / 1000), mod);
+	// } else {
+	// 	ft_printf_fd(1, "%s%i.%i", color, (time / 1000), mod);
+	// }
+
+	dprintf(1, "%s%li.%03i", color, (time / 1000), mod);
 
 	if (!last) {
 		ft_printf_fd(1, "/");
