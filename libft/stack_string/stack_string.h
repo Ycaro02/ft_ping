d94607923fcdab690c9b0832eb0ddb94a13371f2
Ycaro02 <ycaro@Kali.home>
2024-04-08 15:08:04 -0500

    comment stack string

--- libft/stack_string/stack_string.h
+++ libft/stack_string/stack_string.h
@@ -3,9 +3,12 @@
 
 #include "../libft.h"
 
+/* Buffer size of stack string */
+#define SSTRING_MAX 1024
 
-# define SSTRING_MAX 1024
-
+/**
+ * Stack string structure
+*/
 typedef struct s_stack_string
 {
     char        data[SSTRING_MAX];      /* char buffer */
@@ -14,11 +17,43 @@ typedef struct s_stack_string
 
 typedef t_stack_string t_sstring;
 
+/**
+ * @brief Create an empty stack string
+*/
 t_sstring   empty_sstring();
+
+/**
+ * @brief Fill a stack string with a string
+ * @param str The string to fill the stack string with
+*/
 t_sstring   fill_sstring(char *str);
+
+/**
+ * @brief Fill a stack string with multiple strings
+ * @param str1 The first string to fill the stack string with
+ * @param str2 The second string to fill the stack string with
+ * @param str3 The third string to fill the stack string with
+*/
+
 t_sstring   fill_multiple_sstring(char *str1, char *str2, char *str3);
+
+/**
+ * @brief Clear a stack string
+ * @param sstr Pointer on the stack string to clear
+*/
 void        clear_sstring(t_sstring *sstr);
+
+/**
+ * @brief Push a char in a stack string
+ * @param sstr Pointer on the stack string to push the char in
+*/
 void        push_sstring(t_sstring *sstr, char c);
+
+/**
+ * @brief Concatenate a string to a stack string
+ * @param sstr Pointer on the stack string to concatenate the string to
+ * @param str The string to concatenate
+*/
 void        concat_sstring(t_sstring *sstr, char *str);
 
 #endif /* STACK_STRING_H */
