d8df5f759903cf6f90397dee627a75a84e38dce2
Ycaro02 <Ycaro02@hotmail.fr>
2024-04-08 12:06:31 +0200

    handle time comment

--- src/handle_time.c
+++ src/handle_time.c
@@ -1,16 +1,14 @@
 #include "../include/ft_ping.h"
 
+/**
+ * @brief Display time in milliseconds
+ * @param color color to display
+ * @param time time in microseconds
+ * @param last if it's the last time to display
+*/
 void display_ms_time(char *color, suseconds_t time, uint8_t last)
 {
 	uint32_t mod = time % 1000;
-
-	// if (mod < 100) {
-	// 	ft_printf_fd(1, "%s%i.0%i", color, (time / 1000), mod);
-	// } else if (mod < 10) {
-	// 	ft_printf_fd(1, "%s%i.00%i", color, (time / 1000), mod);
-	// } else {
-	// 	ft_printf_fd(1, "%s%i.%i", color, (time / 1000), mod);
-	// }
 
 	dprintf(1, "%s%li.%03i", color, (time / 1000), mod);
 
@@ -21,11 +19,15 @@ void display_ms_time(char *color, suseconds_t time, uint8_t last)
 	}
 }
 
+/**
+ * @brief Get current time in microseconds
+ * @return current time in microseconds
+*/
 suseconds_t  get_ms_time(void)
 {
-        t_timeval  now;
+	t_timeval  now;
 
-        if (gettimeofday(&now, NULL) != 0)
-                return (0);
-        return ((now.tv_sec * 1000000) + now.tv_usec);
+	if (gettimeofday(&now, NULL) != 0)
+			return (0);
+	return ((now.tv_sec * 1000000) + now.tv_usec);
 }
